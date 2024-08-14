5fe43cfb5a3a044680b046604381f6c7d9bee408
Jose Assis <josenunesra@gmail.com>
2024-08-13 22:38:14 -0300

    remove pre-commit

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,34 +1,7 @@
 #!/bin/bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+poetry install
 
-npm install commitizen -g
-commitizen init cz-conventional-changelog --save-dev --save-exact
+poetry update
 
-npm install --save-dev @commitlint/cli
-npm install --save-dev @commitlint/config-conventional
-echo "module.exports = {extends: ['@commitlint/config-conventional']}" > commitlint.config.js
-
-npm install husky --save-dev
-npx husky install
-
-if [ ! -f ".husky/commit-msg" ]
-then
-    npx husky add .husky/commit-msg 'npx --no -- commitlint --edit "$1"'
-fi
-
-if [ ! -f ".husky/prepare-commit-msg" ]
-then
-    npx husky add .husky/prepare-commit-msg "exec < /dev/tty && git cz --hook || true"
-fi
-
-npm install --save-dev @apidevtools/swagger-cli
-
-npm install
-
-# create poetry env and install deps
-npm run thaw
-
-npm run update
+git config --local core.editor "vi"
