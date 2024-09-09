b7b7ed7b8dfe196a695b3ac27226cd6d4fb0801e
Jose Assis <josenunesra@gmail.com>
2024-09-09 18:17:40 +0000

    updates er diagram script

--- generate_er_diagram.py
+++ generate_er_diagram.py
@@ -3,10 +3,10 @@ from sqlalchemy_data_model_visualizer import (
     generate_data_model_diagram,
 )
 
-from app.models import Actor, ActorMovie, Address, Movie
+from app.models import Actor, ActorMovie, Address, Movie, User
 
 # Suppose these are your SQLAlchemy data models defined above in the usual way, or imported from another file:
-models = [Actor, ActorMovie, Address, Movie]
+models = [Actor, ActorMovie, Address, Movie, User]
 output_file_name = "diagrams/my_data_model_diagram"
 generate_data_model_diagram(models, output_file_name)
 add_web_font_and_interactivity(
