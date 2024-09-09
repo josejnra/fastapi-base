cb21ef9abc14db667b66bb615208fe828f8aa821
Jose Assis <josenunesra@gmail.com>
2024-09-09 18:14:20 +0000

    clean up

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -1,10 +1,8 @@
-from datetime import timedelta
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 
-from app.core.config import get_settings
 from app.core.database import SessionDep
 from app.core.security import (
     CurrentActiveUserDep,
@@ -28,10 +26,13 @@ async def login_for_access_token(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=get_settings().ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
+    access_token = create_access_token(data={"sub": user.username})
+    return Token(access_token=access_token, token_type="bearer")
+
+
+@router.post("/refresh", status_code=status.HTTP_200_OK, response_model=Token)
+async def refresh_token(user: CurrentActiveUserDep):
+    access_token = create_access_token(data={"sub": user.username})
     return Token(access_token=access_token, token_type="bearer")
 
 
